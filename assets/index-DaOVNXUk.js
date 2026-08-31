0bf072dcfed8399db58eb41afb441dbd841aa077
Cha-NPE <Cha-NPE@users.noreply.github.com>
2026-08-31 01:53:43 +0000

    deploy: 34775e13cc21d3014ed2bea53fa55ec639761cc7

--- assets/index-DaOVNXUk.js
+++ assets/index-DaOVNXUk.js
@@ -223,7 +223,7 @@ end`},Yi=function(){return`<`+[...arguments].join(``)+`>`},Xi=function(e){return
         <div><strong>Connect [size]mm ${{one:`1`,two:`2`,three:`3`}[C.toLowerCase()]||C}C NS service cable to ${T===`Pillar`?`Pillar Box# `+E:T+`# `+E}</strong></div>
         <div><strong>COC, ROI?, & lock off photo attached</strong></div>
         <div><strong>Permanent/Temporary: ${y}</strong></div>
-        <div><strong>J A Russell PO# 459XXX</strong></div>
+        <div><strong>J A Russell PO# 460XXX</strong></div>
         ${ce?`<div><strong>Asset missing</strong></div>`:``}
         ${le?`<div><strong>Asset number missing</strong></div>`:``}
         `}
